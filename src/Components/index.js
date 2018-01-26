4eeea7175f361d7faba88a9441ebbdac036fccfd
Dhansuhu Uzumaki <a.p.dhanushu@gmail.com>
2018-01-26 22:01:25 +0530

    add recipe button

--- src/Components/index.js
+++ src/Components/index.js
@@ -3,23 +3,28 @@ import Header from './Header';
 import Footer from './Footer';
 import AccordionCard from './AccordionCard';
 
-const App = () => (
-  <div>
-    <Header />
-    <div className="accordion-cards">
-      <AccordionCard />
-      <AccordionCard />
-      <AccordionCard />
-      <AccordionCard />
-      <AccordionCard />
-      <AccordionCard />
-      <AccordionCard />
-      <AccordionCard />
-      <AccordionCard />
-      <AccordionCard />
-    </div>
-    <Footer />
-  </div>
-);
+class App extends React.Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <div className="add-recipe"><button>+</button></div>
+        <div className="accordion-cards">
+          <AccordionCard />
+          <AccordionCard />
+          <AccordionCard />
+          <AccordionCard />
+          <AccordionCard />
+          <AccordionCard />
+          <AccordionCard />
+          <AccordionCard />
+          <AccordionCard />
+          <AccordionCard />
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
 
 export default App;
