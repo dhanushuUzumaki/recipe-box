8d9a57a96fea0afd55048a80a4974dc8588cbe62
Dhansuhu Uzumaki <a.p.dhanushu@gmail.com>
2018-02-06 22:40:51 +0530

    initial version

--- src/Components/index.js
+++ src/Components/index.js
@@ -1,4 +1,4 @@
-/* global localStorage */
+/* global localStorage alert */
 
 import React from 'react';
 import Header from './Header';
@@ -13,10 +13,12 @@ class App extends React.Component {
     super(props);
     this.state = {
       showAddRecipeModal: false,
+      showEditRecipeModal: false,
       currentRecipe: {
         recipeName: '',
         ingredients: ''
-      }
+      },
+      currentRecipeId: -1
     };
     /* eslint-disable */
     this.showAddRecipeModal = e => this._showAddRecipeModal();
@@ -27,6 +29,12 @@ class App extends React.Component {
     this.renderCards = () => this._renderCards();
     this.loadAndUpdateFromLocalStorage = () => this._loadAndUpdateFromLocalStorage();
     this.updateLocalStorage = () => this._updateLocalStorage();
+    this.renderAddRecipeModal = () => this._renderAddRecipeModal();
+    this.renderEditRecipeModal = () => this._renderEditRecipeModal();
+    this.deleteRecipe = index => this._deleteRecipe(index);
+    this.updateRecipe = () => this._updateRecipe();
+    this.showEditRecipeModal = index => this._showEditRecipeModal(index);
+    this.closeEditRecipeModal = e => this._closeEditRecipeModal();
     /* eslint-enable */
   }
 
@@ -36,7 +44,7 @@ class App extends React.Component {
 
   _loadAndUpdateFromLocalStorage () {
     let recipes = JSON.parse(localStorage.getItem('recipies'));
-    if (recipes === null) {
+    if (recipes === null || recipes.length < 1) {
       recipes = [{
         recipeName: 'Poached Egg',
         ingredients: 'Eggs, Water, Vinegar'
@@ -71,6 +79,29 @@ class App extends React.Component {
     });
   }
 
+  _showEditRecipeModal (index) {
+    this.setState((prevState) => {
+      const currentRecipe = prevState.recipes[index];
+      return {
+        showEditRecipeModal: true,
+        currentRecipeId: index,
+        currentRecipe
+      };
+    });
+  }
+
+  _closeEditRecipeModal () {
+    const currentRecipe = {
+      recipeName: '',
+      ingredients: ''
+    };
+    this.setState({
+      showEditRecipeModal: false,
+      currentRecipe,
+      currentRecipeId: -1
+    });
+  }
+
   _onRecipeNameChange (value) {
     const currentRecipe = Object.assign({}, this.state.currentRecipe, { recipeName: value });
     this.setState({
@@ -87,22 +118,130 @@ class App extends React.Component {
 
   _addRecipe () {
     let newState = JSON.parse(JSON.stringify(this.state)); // eslint-disable-line prefer-const
-    newState.recipes.push(newState.currentRecipe);
-    this.setState(newState, () => {
-      this.updateLocalStorage();
-      this.closeAddRecipeModal();
-    });
+    const { recipeName, ingredients } = newState.currentRecipe;
+    if (recipeName !== '' && ingredients !== '') {
+      newState.recipes.push(newState.currentRecipe);
+      this.setState(newState, () => {
+        this.updateLocalStorage();
+        this.closeAddRecipeModal();
+      });
+    } else {
+      setTimeout(() => alert('Enter Recipe to add.'), 500); // eslint-disable-line no-alert
+    }
+  }
+
+  _deleteRecipe (index) {
+    const newState = JSON.parse(JSON.stringify(this.state));
+    newState.recipes.splice(index, 1);
+    this.setState(newState, this.updateLocalStorage);
+  }
+
+  _updateRecipe () {
+    const newState = JSON.parse(JSON.stringify(this.state));
+
+    const { recipeName, ingredients } = newState.currentRecipe;
+    if (recipeName !== '' && ingredients !== '') {
+      newState.recipes[this.state.currentRecipeId] = newState.currentRecipe;
+      this.setState(newState, () => {
+        this.updateLocalStorage();
+        this.closeEditRecipeModal();
+      });
+    } else {
+      setTimeout(() => alert('Can\'t save empty recipe.'), 500); // eslint-disable-line no-alert
+    }
   }
 
   _renderCards () {
     const { recipes } = this.state;
-    return recipes.map(recipe => (
-      <AccordionCard
-        header={recipe.recipeName}
+    let content;
+    if (recipes.length > 0) {
+      content = recipes.map((recipe, index) => (
+        <AccordionCard
+          header={recipe.recipeName}
+        >
+          <span className="side-heading">Ingredients</span>: {recipe.ingredients}
+          <div>
+            <button
+              className="btn btn-primary ripple"
+              onClick={() => this.showEditRecipeModal(index)}
+            >
+              Edit
+            </button>
+            <button
+              className="btn btn-delete ripple"
+              onClick={() => this.deleteRecipe(index)}
+            >
+              Delete
+            </button>
+          </div>
+        </AccordionCard>
+      ));
+    } else {
+      content = <h5>Add Recipies and cook your way to the top ;)</h5>;
+    }
+
+    return content;
+  }
+
+  _renderAddRecipeModal () {
+    return (
+      <Modal
+        onClose={this.closeAddRecipeModal}
+        header="Add Recipe"
+        isVisible={this.state.showAddRecipeModal}
       >
-        {recipe.ingredients}
-      </AccordionCard>
-    ));
+        <Input
+          type="text"
+          placeholder="Recipie Name"
+          onChange={this.onRecipeNameChange}
+          identifier="recipeName"
+          value={this.state.currentRecipe.recipeName}
+        />
+        <TextArea
+          placeholder="Ingredients (comma separated)"
+          onChange={this.onIngredientsChange}
+          identifier="ingredients"
+          value={this.state.currentRecipe.ingredients}
+        />
+        <div className="footer-btns">
+          <button className="btn btn-link" onClick={this.closeAddRecipeModal}>Cancel</button>
+          <button className="btn btn-primary ripple" onClick={this.addRecipe}>Add</button>
+        </div>
+      </Modal>
+    );
+  }
+
+  _renderEditRecipeModal () {
+    return (
+      <Modal
+        onClose={this.closeEditRecipeModal}
+        header="Add Recipe"
+        isVisible={this.state.showEditRecipeModal}
+      >
+        <Input
+          type="text"
+          placeholder="Recipie Name"
+          onChange={this.onRecipeNameChange}
+          identifier="recipeName"
+          value={this.state.currentRecipe.recipeName}
+        />
+        <TextArea
+          placeholder="Ingredients (comma separated)"
+          onChange={this.onIngredientsChange}
+          identifier="ingredients"
+          value={this.state.currentRecipe.ingredients}
+        />
+        <div className="footer-btns">
+          <button className="btn btn-link" onClick={this.closeEditRecipeModal}>Cancel</button>
+          <button
+            className="btn btn-primary ripple"
+            onClick={this.updateRecipe}
+          >
+            Save
+          </button>
+        </div>
+      </Modal>
+    );
   }
 
   render () {
@@ -115,29 +254,8 @@ class App extends React.Component {
         <div className="accordion-cards">
           {this.renderCards()}
         </div>
-        <Modal
-          onClose={this.closeAddRecipeModal}
-          header="Add Recipe"
-          isVisible={this.state.showAddRecipeModal}
-        >
-          <Input
-            type="text"
-            placeholder="Recipie Name"
-            onChange={this.onRecipeNameChange}
-            identifier="recipeName"
-            value={this.state.currentRecipe.recipeName}
-          />
-          <TextArea
-            placeholder="Ingredients (comma separated)"
-            onChange={this.onIngredientsChange}
-            identifier="ingredients"
-            value={this.state.currentRecipe.ingredients}
-          />
-          <div className="footer-btns">
-            <button className="btn btn-link" onClick={this.closeAddRecipeModal}>Cancel</button>
-            <button className="btn btn-primary ripple" onClick={this.addRecipe}>Add</button>
-          </div>
-        </Modal>
+        {this.renderAddRecipeModal()}
+        {this.renderEditRecipeModal()}
         <Footer />
       </div>
     );
